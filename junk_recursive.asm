0c98e133159bfe846d6375dc2e0d36d6f4eaad60
Breno Van Dall <130492634+brenovandall@users.noreply.github.com>
2025-03-11 22:34:46 -0300

    ..

--- junk_recursive.asm
+++ junk_recursive.asm
@@ -2,17 +2,17 @@ section .text
     global _start
 
 _start:
-    test rdi,rdi
-    je   .L6
-    push rbx
-    mov  rbx,rdi
-    and  ebx,1
-    shr  rdi,1
-    call _start
-    add  rbx,rax
-    pop  rbx
-    ret
+        test rdi,rdi
+        je   .L6
+        push rbx
+        mov  rbx,rdi
+        and  ebx,1
+        shr  rdi,1
+        call _start
+        add  rbx,rax
+        pop  rbx
+        ret
 .L6:
-    mov rax,60
-    xor rdi,rdi
-    syscall
+        mov rax,60
+        xor rdi,rdi
+        syscall
